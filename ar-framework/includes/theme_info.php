2b87625b44ec9cece160d2dce497e8ff3ab7ee4d
mamb2b <mamb2b@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-22 05:56:50 +0000

    First Release of Plugin
    git-svn-id: https://plugins.svn.wordpress.org/ar-simple-social-share/tags/1.0@826870 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ar-framework/includes/theme_info.php
@@ -0,0 +1,6 @@
+<?php
+define('PLUGIN_NAME', 'AR Simple Social Share Plugin');
+define('PLUGIN_AUTHOR', 'AR WP Themes');
+define('PLUGIN_VERSION', '1.0');
+define('PLUGIN_FRAMEWORK_NAME', 'AR Framework');
+?>
